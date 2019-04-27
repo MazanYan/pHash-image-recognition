dda23d2b3c11ffdb240bb722fcf3b4b8cff2887e
MazanYan <mazanyan027@gmail.com>
2019-04-27 21:04:22 +0300

    Building hash added

--- raw_algorithms_python/main.py
+++ raw_algorithms_python/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from PIL import Image
 import numpy as np
 from tests.test_functions import *
@@ -14,13 +16,17 @@ def compress(image: "Image"):
     rows_to_leave = range(0, height+1, int(height/7))
     columns_to_leave = range(0, width+1, int(width/7))
     image_array = np.array(image)
-    new_image_array = np.array([np.array([image_array[i][j] if len(image_array[i][j]) == 3 else image_array[i][j][:3] for j in columns_to_leave]) for i in rows_to_leave])
+    new_image_array = np.array([np.array([image_array[i][j] if len(image_array[i][j]) == 3 else image_array[i][j][:3]
+                                          for j in columns_to_leave])
+                                for i in rows_to_leave])
     return new_image_array
 
 
 def two_colored(compressed_image):
-    new_array = np.array([np.array([np.array([0, 0, 0]) if np.linalg.norm(compressed_image[i][j]) < 128 else np.array([255, 255, 255]) for j in range(len(compressed_image))]) for i in range(len(compressed_image[0]))])
-    return (new_array.astype(np.uint8))
+    new_array = np.array([np.array([np.array([0, 0, 0]) if np.linalg.norm(compressed_image[i][j]) < 128 else np.array([255, 255, 255])
+                                    for j in range(len(compressed_image))])
+                          for i in range(len(compressed_image[0]))])
+    return new_array.astype(np.uint8)
 
 
 def save_compressed(compressed_array: "np.array np.uint8 type", name_to_save):
@@ -28,6 +34,11 @@ def save_compressed(compressed_array: "np.array np.uint8 type", name_to_save):
     image.save(str(name_to_save))
 
 
-print(__name__)
+def build_hash(compressed_array: "two colors image array"):
+    all_array_points = np.array(reduce(lambda res_array, row: res_array + list(row), compressed_array, []))
+    bytearr = "".join(["\0" if (el - np.array([255, 255, 255])).any() else "\1" for el in all_array_points])
+    return bytearr.encode()
+
+
 if __name__ == "__main__":
-    test()
+    Test().test_encoding()
--- raw_algorithms_python/tests/test_functions.py
+++ raw_algorithms_python/tests/test_functions.py
@@ -1,17 +1,55 @@
 from main import *
 import os
+import unittest
 
 
-def test():
-    get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
-    hashed_path =  os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
+class Test(unittest.TestCase):
 
-    random_drawing = compress(Image.open(get_image_path + "Untitled.png"))
-    parrot_image = compress(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
-    letter_ts = compress(Image.open(os.path.abspath(get_image_path + "Ц.png")))
-    comic_con_photo = compress(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+    def test_compressing(self):
+        get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
+        hashed_path = os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
 
-    save_compressed(two_colored(random_drawing), hashed_path + "new random.png")
-    save_compressed(two_colored(parrot_image), hashed_path + "new parrot.png")
-    save_compressed(two_colored(letter_ts), hashed_path + "new letter.png")
-    save_compressed(two_colored(comic_con_photo),hashed_path + "new comic con photo.png")
+        try:
+            random_drawing = compress(Image.open(get_image_path + "Untitled.png"))
+            parrot_image = compress(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
+            letter_ts = compress(Image.open(os.path.abspath(get_image_path + "Ц.png")))
+            comic_con_photo = compress(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+            print("Images compress")
+        except:
+            self.assertTrue(False)
+
+    def test_saving(self):
+        get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
+        hashed_path =  os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
+
+        random_drawing = compress(Image.open(get_image_path + "Untitled.png"))
+        parrot_image = compress(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
+        letter_ts = compress(Image.open(os.path.abspath(get_image_path + "Ц.png")))
+        comic_con_photo = compress(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+
+        try:
+            save_compressed(two_colored(random_drawing), hashed_path + "new random.png")
+            save_compressed(two_colored(parrot_image), hashed_path + "new parrot.png")
+            save_compressed(two_colored(letter_ts), hashed_path + "new letter.png")
+            save_compressed(two_colored(comic_con_photo),hashed_path + "new comic con photo.png")
+            save_compressed(two_colored(random_drawing), hashed_path + "new random.png")
+            print("Compressed images save")
+        except Exception:
+            self.assertTrue(False)
+
+    def test_encoding(self):
+        get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
+        hashed_path = os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
+
+        random_drawing = compress(Image.open(get_image_path + "Untitled.png"))
+        parrot_image = compress(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
+        letter_ts = compress(Image.open(os.path.abspath(get_image_path + "Ц.png")))
+        comic_con_photo = compress(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+
+        try:
+            self.assertIsInstance(build_hash(random_drawing), bytes)
+            self.assertIsInstance(build_hash(parrot_image), bytes)
+            self.assertIsInstance(build_hash(letter_ts), bytes)
+            self.assertIsInstance(build_hash(comic_con_photo), bytes)
+        except:
+            raise AssertionError
