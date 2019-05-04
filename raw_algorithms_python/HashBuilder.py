b7b8a3c60ef8b44f4eb85bc98eaa5413cc7e0747
MazanYan <mazanyan027@gmail.com>
2019-05-04 09:54:54 +0300

    Hash now depends of average brightness. Docs added

--- raw_algorithms_python/HashBuilder.py
+++ raw_algorithms_python/HashBuilder.py
@@ -4,10 +4,19 @@ from PIL import Image
 import numpy as np
 
 
+# Class to get images from some folder and hash them with simple pHash algorithm
+
 class HashBuilder():
     def __init__(self, path):
         self.images_path = path
 
+    """
+    Compresses image object into 8x8 square array to be used in saving as a new file or recoloring as two-colored image
+    for further hash building
+    The method saves initial colors with saving only each 7th row/column of image array
+    :param image an object of Image class
+    :return 8x8 numpy image array of uint8 type
+    """
     @staticmethod
     def __compress__(image: "Image"):
         width, height = image.size
@@ -18,21 +27,45 @@ class HashBuilder():
             [np.array([image_array[i][j] if len(image_array[i][j]) == 3 else image_array[i][j][:3]
                        for j in columns_to_leave])
              for i in rows_to_leave])
-        return new_image_array
+        return new_image_array.astype(np.uint8)
 
+    """
+    Rewrites compressed image 8x8 array making him have only two colors - black and white
+    Finds average brightness of image and discretizes all other colors,
+        making each point with smaller brightness black
+        and making each point with greater brightness white
+    :param compressed_image 8x8 numpy image array of uint8 type that is already compressed by __compress__ method
+    :return two-colored 8x8 numpy image array of uint8 type
+    """
     @staticmethod
     def __two_colored__(compressed_image):
+        all_array_points = np.array(reduce(lambda res_array, row: res_array + list(row), compressed_image, []))
+        average_brightness = np.linalg.norm(sum(all_array_points))
         new_array = np.array([np.array(
-            [np.array([0, 0, 0]) if np.linalg.norm(compressed_image[i][j]) < 128 else np.array([255, 255, 255])
+            [np.array([0, 0, 0]) if np.linalg.norm(compressed_image[i][j]) < average_brightness else np.array([255, 255, 255])
              for j in range(len(compressed_image))])
                               for i in range(len(compressed_image[0]))])
         return new_array.astype(np.uint8)
 
+    """
+    Saves compressed image as image file. Is used for tests only
+    :param compressed_array 8x8 numpy image array of uint8 type
+    :param name_to_save image name with path to be saved in
+    :return None
+    """
     @staticmethod
     def __save_compressed__(compressed_array: "np.array np.uint8 type", name_to_save):
         image = Image.fromarray(compressed_array.astype(np.uint8))
         image.save(str(name_to_save))
 
+    """
+    Builds hash of compressed two colored 8x8 numpy image array
+    Hashes each point into bytearray depending of its' color:
+        black point gets hashed into \0 symbol,
+        white point gets hashed into \1 symbol
+    :param compressed_array 8x8 numpy image array of uint8 type
+    :return bytearray sequence of \0 and \1 symbols
+    """
     @staticmethod
     def __build_hash__(compressed_array: "two colors image array"):
         all_array_points = np.array(reduce(lambda res_array, row: res_array + list(row), compressed_array, []))
@@ -40,4 +73,8 @@ class HashBuilder():
         return bytearr.encode()
 
     def hash_image(self, image_name: "str"):
-        return self.__build_hash__(self.__two_colored__(self.__compress__(Image.open(self.images_path + image_name))))
+        return self.images_path + image_name, self.__build_hash__(self.__two_colored__(self.__compress__(Image.open(self.images_path + image_name))))
+
+
+class CheckHash():
+    pass
--- raw_algorithms_python/tests/test_functions.py
+++ raw_algorithms_python/tests/test_functions.py
@@ -12,33 +12,28 @@ class Test(unittest.TestCase):
         self.test_class = HashBuilder(self.get_image_path)
 
     def test_compressing(self):
-        get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
-        hashed_path = os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
-
         try:
-            self.test_class.__compress__(Image.open(get_image_path + "Untitled.png"))
-            self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
-            self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "Ц.png")))
-            self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+            self.test_class.__compress__(Image.open(self.get_image_path + "Untitled.png"))
+            self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "photo_2018-12-10_22-28-40.jpg")))
+            self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "Ц.png")))
+            self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "kiev comic conS80922-141312.jpg")))
             print("Images compress")
         except:
             self.assertTrue(False)
 
     def test_saving(self):
-        get_image_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
-        hashed_path =  os.path.dirname(os.path.realpath(__file__)) + "/hashed_images/"
-
-        random_drawing = self.test_class.__compress__(Image.open(get_image_path + "Untitled.png"))
-        parrot_image = self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "photo_2018-12-10_22-28-40.jpg")))
-        letter_ts = self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "Ц.png")))
-        comic_con_photo = self.test_class.__compress__(Image.open(os.path.abspath(get_image_path + "kiev comic conS80922-141312.jpg")))
+        random_drawing = self.test_class.__compress__(Image.open(self.get_image_path + "Untitled.png"))
+        parrot_image = self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "photo_2018-12-10_22-28-40.jpg")))
+        new_parrot_image = self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "edited photo_2018-12-10_22-28-40.jpg")))
+        letter_ts = self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "Ц.png")))
+        comic_con_photo = self.test_class.__compress__(Image.open(os.path.abspath(self.get_image_path + "kiev comic conS80922-141312.jpg")))
 
         try:
-            self.test_class.__save_compressed__(self.test_class.__two_colored__(random_drawing), hashed_path + "new random.png")
-            self.test_class.__save_compressed__(self.test_class.__two_colored__(parrot_image), hashed_path + "new parrot.png")
-            self.test_class.__save_compressed__(self.test_class.__two_colored__(letter_ts), hashed_path + "new letter.png")
-            self.test_class.__save_compressed__(self.test_class.__two_colored__(comic_con_photo),hashed_path + "new comic con photo.png")
-            self.test_class.__save_compressed__(self.test_class.__two_colored__(random_drawing), hashed_path + "new random.png")
+            self.test_class.__save_compressed__(self.test_class.__two_colored__(random_drawing), self.hashed_path + "new random.png")
+            self.test_class.__save_compressed__(self.test_class.__two_colored__(parrot_image), self.hashed_path + "new parrot.png")
+            self.test_class.__save_compressed__(self.test_class.__two_colored__(new_parrot_image), self.hashed_path + "new edited parrot.png")
+            self.test_class.__save_compressed__(self.test_class.__two_colored__(letter_ts), self.hashed_path + "new letter.png")
+            self.test_class.__save_compressed__(self.test_class.__two_colored__(comic_con_photo), self.hashed_path + "new comic con photo.png")
             print("Compressed images save")
         except Exception:
             self.assertTrue(False)
@@ -46,14 +41,16 @@ class Test(unittest.TestCase):
     def test_hashing(self):
         random_drawing = "Untitled.png"
         parrot_image = "photo_2018-12-10_22-28-40.jpg"
+        new_parrot_image = "edited photo_2018-12-10_22-28-40.jpg"
         letter_ts = "Ц.png"
         comic_con_photo = "kiev comic conS80922-141312.jpg"
 
         try:
-            self.assertIsInstance(self.test_class.hash_image(random_drawing), bytes)
-            self.assertIsInstance(self.test_class.hash_image(parrot_image), bytes)
-            self.assertIsInstance(self.test_class.hash_image(letter_ts), bytes)
-            self.assertIsInstance(self.test_class.hash_image(comic_con_photo), bytes)
+            self.assertIsInstance(self.test_class.hash_image(random_drawing)[1], bytes)
+            self.assertIsInstance(self.test_class.hash_image(parrot_image)[1], bytes)
+            self.assertIsInstance(self.test_class.hash_image(letter_ts)[1], bytes)
+            self.assertIsInstance(self.test_class.hash_image(comic_con_photo)[1], bytes)
+            self.assertIsInstance(self.test_class.hash_image(new_parrot_image)[1], bytes)
             print("Images get hashed")
         except:
             raise AssertionError
